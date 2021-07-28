4de02d40abbc0f3ffea03f130cad0a48e9d1566a
mklavuhn <michaelklavuhn3@gmail.com>
2021-07-28 10:46:17 -0700

    commit tree eb3eff10ed7344477ed3d05b854021faa8122fe4

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def line(array)
+    if array.length == 0 
+        puts "The line is currently empty."
+    else 
+        phrase = "The line is currently:"
+        array.each_with_index do |name, number|
+            phrase += " #{number + 1}. #{name}"
+        end
+        puts "#{phrase}"
+    end
+end
+
+def take_a_number(katz_deli, name)
+        katz_deli << name
+        phrase = "Welcome, #{name}. You are number #{katz_deli.length} in line."
+        puts "#{phrase}"
+end
+
+def now_serving(katz_deli)
+    if katz_deli.length == 0 
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{katz_deli[0]}."
+        katz_deli.delete_at(0)
+    end
+end
+
+
+
+
+
